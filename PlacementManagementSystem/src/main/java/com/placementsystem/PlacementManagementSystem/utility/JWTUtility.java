22b40b49cf598c8ec44cf977d88f88748834d0ac
MahimaAwasthi <40446284+MahimaAwasthi@users.noreply.github.com>
2023-08-27 17:45:19 +0530

    Update JWTUtility.java

--- PlacementManagementSystem/src/main/java/com/placementsystem/PlacementManagementSystem/utility/JWTUtility.java
+++ PlacementManagementSystem/src/main/java/com/placementsystem/PlacementManagementSystem/utility/JWTUtility.java
@@ -19,7 +19,7 @@ public class JWTUtility implements Serializable {
 
     private static final long serialVersionUID = 234234523523L;
 
-    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+    public static final long JWT_TOKEN_VALIDITY = (1 * 60 * 60) / 2;
 
     @Value("${jwt.secret}")
     private String secretKey;
